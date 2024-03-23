dbf85ed84afc1c7d9fb4c113c4b9860308d9c3f1
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-23 10:28:11 +0100

    added auto increment and id type

--- src/pyrepositories/datasource.py
+++ src/pyrepositories/datasource.py
@@ -1,8 +1,32 @@
 from .datatable import DataTable
+from .lib import Entity
+import random
+import string
+
+
+def get_random_string_id():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+
+
+def get_sequential_id(data):
+    return len(data) + 1
+
+
+def get_id(id_type, data):
+    if id_type == int:
+        return get_sequential_id(data)
+    else:
+        return get_random_string_id()
+
 
 class DataSource:
-    def __init__(self):
+    def __init__(self, auto_increment=True, id_type=int):
+        if id_type not in [int, str]:
+            raise ValueError("id_type must be either int or str")
+
+        self.id_type = id_type
         self.tables = [] #type: list[DataTable]
+        self.auto_increment = auto_increment
 
     def add_table(self, table: DataTable):
         if table not in self.tables:
@@ -26,7 +50,7 @@ class DataSource:
         else:
             return None
 
-    def get_by_id(self, table_name: str, id):
+    def get_by_id(self, table_name: str, id: int | str):
         table = self.get_table(table_name)
         if table:
             return table.get_by_id(id)
@@ -40,14 +64,21 @@ class DataSource:
         else:
             return None
 
-    def insert(self, table_name: str, data):
+    def insert(self, table_name: str, data: Entity):
         table = self.get_table(table_name)
-        if table:
-            return table.insert(data)
-        else:
-            return None
+        if not table:
+            raise ValueError("Table not found")
 
-    def update(self, table_name, id, data):
+        if self.auto_increment:
+            data.id = get_id(self.id_type, table.get_all())
+        else:
+            if not data.id:
+                raise ValueError("Entity must have an id")
+            if table.get_by_id(data.id):
+                return False
+        return table.insert(data)
+
+    def update(self, table_name, id: int | str, data: Entity):
         table = self.get_table(table_name)
         if table:
             return table.update(id, data)
