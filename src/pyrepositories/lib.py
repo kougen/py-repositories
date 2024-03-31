6c70a0b8511975ef2ecbeb237bc5fddbe54bc504
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-31 23:46:40 +0200

    added fieldtype classes

--- src/pyrepositories/lib.py
+++ src/pyrepositories/lib.py
@@ -7,21 +7,88 @@ class IdTypes(Enum):
     STR = str
     UUID = 'uuid'
 
+class FieldKeyTypes(Enum):
+    PRIMARY = 'primary'
+    UNIQUE = 'unique'
+    STANDARD = 'standard'
+    REQUIRED = 'required'
+    OPTIONAL = 'optional'
+
+class FieldTypes(Enum):
+    INT = int
+    STR = str
+    UUID = 'uuid'
+    BOOL = bool
+    FLOAT = float
+    LIST = list
+    DICT = dict
+
+class FieldValue:
+    def __init__(self, value: Any, entity_id: IdTypes):
+        self.entity_id = entity_id
+        self.value = value
+
+class Field:
+    def __init__(self, name: str, field_type: FieldTypes, key_type: FieldKeyTypes, default: Any = None):
+        self.name = name
+        self.field_type = field_type
+        self.key_type = key_type
+        self.default = default
+        if key_type == FieldKeyTypes.OPTIONAL and default is None:
+            raise ValueError(f"Field {name} is optional but has no default value")
+        self.values = {}  # type: dict[IdTypes, FieldValue]
+
+    def set_value(self, entity_id: IdTypes, value: Any = None):
+        if self.key_type == FieldKeyTypes.REQUIRED and value is None:
+            raise ValueError(f"Field {self.name} is required")
+
+        if self.key_type == FieldKeyTypes.UNIQUE:
+            if value in self.values.values():
+                raise ValueError(f"Value {value} already exists in field {self.name}")
+
+        self.values[entity_id] = FieldValue(value, entity_id)
+
+    def get_value(self, entity_id: IdTypes) -> Any | None:
+        return self.values.get(entity_id)
+
+    def __str__(self):
+        return f"{self.name} ({self.field_type}, {self.key_type})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def serialize(self) -> dict[str, tuple]:
+        return {
+            self.name : (self.field_type, self.key_type, self.default)
+        }
+
+class EntityField:
+    def __init__(self, name: str, field_type: FieldTypes, key_type: FieldKeyTypes, value: Any = None, default: Any = None):
+        self.name = name
+        self.field_type = field_type
+        self.key_type = key_type
+        self.default = default
+        self.value = value
+
+    def serialize(self) -> dict[str, tuple]:
+        return {
+            self.name : (self.field_type, self.key_type, self.default)
+        }
 
 class Entity:
-    def __init__(self, id: int | str | None = None):
+    def __init__(self, id: IdTypes | None = None):
         self.id = id
-        self.fields = {}
+        self.fields = {}  # type: dict[str, EntityField]
 
-    def add_field(self, name: str, value: Any):
-        self.fields[name] = value
+    def add_field(self, name: str, field: EntityField):
+        self.fields[name] = field
 
-    def get_field(self, name: str) -> Any:
+    def get_field(self, name: str) -> EntityField | None:
         return self.fields.get(name)
 
     def matches_criteria(self, key: str, value: str) -> bool:
         if isinstance(self.fields[key], str):
-            return value.lower() in self.fields[key].lower()
+            return value.lower() in self.fields[key].value.lower()
         else:
             return value == self.fields[key]
 
@@ -32,16 +99,18 @@ class Entity:
         return True
 
     def serialize(self) -> dict[str, Any]:
-        return {
-            'id': self.id,
-            **self.fields
-        }
+        data = {}
+        data['id'] = self.id
+        for field in self.fields.values():
+            data[field.name] = field.value
+        return data
  
     def __str__(self):
         return str(self.serialize())
 
     def __repr__(self):
         return self.__str__()
+
 
 class FilterField:
     def __init__(self, name, field_type, default=None):
